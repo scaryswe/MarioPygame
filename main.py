4a821b2b62c193fe6fb42bfac75675ce309de12a
Ashraj Grewal <ashraj.grewal10@gmail.com>
2021-03-22 23:22:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ def main():
     run = True
     while run:
         for event in pygame.event.get():
-            
+            if event.type == pygame.QUIT:
+                run == False
+    pygame.quit()
+    if __name__ == "__main__":
+        main()
