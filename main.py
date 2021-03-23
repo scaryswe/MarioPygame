ae03aab7cba08251915729f848a19e4db6383414
Ashraj Grewal <ashraj.grewal10@gmail.com>
2021-03-23 00:14:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("MarioMiniGame")
 
+WHITE=(255,255,255)
+BLACK= (0,0,0)
+
+FPS = 60
+
+def draw_window():
+    WIN.fill((WHITE))
+    pygame.display.update()
+
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run == False
+        draw_window
+
     pygame.quit()
     if __name__ == "__main__":
         main()
