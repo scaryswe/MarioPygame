dde38569a79b71b7ea7140c5541b7c44554c0c44
Ashraj Grewal <ashraj.grewal10@gmail.com>
2021-03-23 08:53:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ WHITE=(255,255,255)
 BLACK= (0,0,0)
 
 FPS = 60
-
+#Need to get this to load in, then resize, then move, then work on collision
 marioCharacterImage = pygame.image.load(os.path.join('assets', 'maryo.png'))
+mario = pygame.transform.scale(marioCharacterImage, (50, 50))
 
 def draw_window():
     WIN.fill(WHITE)
-    WIN.blit(marioCharacterImage,(300, 100))
+    WIN.blit(marioCharacterImage,(mario.x, mario.y))
     pygame.display.update()
 
 def main():
+    #part of function to make mario move
+    mario = pygame.Rect(100,300)
+
     clock = pygame.time.Clock()
     run = True
     while run:
